46f6deac86ff74420b74390496b2370355a5b646
Victor Henrique <102931817+VictorHp21@users.noreply.github.com>
2025-09-28 22:14:25 -0300

    primeira aula prática

---
+++ práticas/PrimeiraAula/app.py
@@ -0,0 +1,31 @@
+print(7 + 4)
+
+print('7' + '4')
+
+idade = 20
+print('Minha idade é: ' + str(idade))
+
+nome = 'Victor'
+print('Meu nome é:' + nome)
+
+peso = 80.5
+
+
+print(nome, idade, peso)
+
+nome = input('Qual seu nome? ')
+idade = input('Qual sua idade? ')
+peso = input('Qual seu peso? ')
+
+print('Seu nome é: ' + nome)
+print('Sua idade é: ' + idade)
+print('Seu peso é: ' + peso)
+
+# receber dois números e somar
+
+n1 =  input('Digite um número: ')
+n2 =  input('Digite outro número: ')
+
+print(int(n1) + int(n2))
+
+
