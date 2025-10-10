ea0e5761cb11bf35788099f58fd6ab65c421b4df
Victor Henrique <102931817+VictorHp21@users.noreply.github.com>
2025-10-09 22:31:12 -0300

    desafio 5 e 6

---
+++ práticas/desafio005/app.py
@@ -0,0 +1,10 @@
+# Antecessor e sucessor.
+
+n = int(input("Digite um valor para descobrir o seu sucessor e antecessor:"))
+
+print('O sucessor é: ', n+1, '\nO antecessor é: ', n-1)
+
+#outra forma seria
+
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, (n-1), (n+1)))
+
