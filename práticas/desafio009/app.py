81e924dabd47c677c9da8ace7bbd930be2c50bbb
Victor Henrique <102931817+VictorHp21@users.noreply.github.com>
2025-10-30 21:54:44 -0300

    desafio 9 e 10

---
+++ práticas/desafio009/app.py
@@ -0,0 +1,9 @@
+n = int(input("Digite um número para ver sua tabuada: "))
+
+print("----------------")
+
+for i in range (1,11):
+    print(f'{n} * {i} = {i * n}')
+
+
+print("----------------")
---
+++ práticas/desafio010/app.py
@@ -0,0 +1,7 @@
+m = float(input('Quanto você tem em sua carteira? R$'))
+
+cotacao_dolar = 5.38
+
+m_dolar = m / cotacao_dolar;
+
+print(f'Com R${m} você pode comprar US${m_dolar}')
